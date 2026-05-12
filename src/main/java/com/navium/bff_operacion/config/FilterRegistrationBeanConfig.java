9cd17ac1e38034caa9f7e040743d3d0657476519
fabetabilo <fab.tabilo@duocuc.cl>
2026-05-12 10:32:40 -0400

    add: cors config

---
+++ src/main/java/com/navium/bff_operacion/config/FilterRegistrationBeanConfig.java
@@ -0,0 +1,30 @@
+package com.navium.bff_operacion.config;
+
+import java.util.List;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class FilterRegistrationBeanConfig {
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // temporal!!!
+        config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(0); // lanza antes que Spring Security. no desaactiva, agrega headers cors y permtir que el preflight llegue a cliente
+        return bean;
+    }
+}
